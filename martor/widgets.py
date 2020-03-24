184e6fe40c47dd060f64aaab00a581dffc0836a1
Tudor Brindus <me@tbrindus.ca>
2020-03-24 16:23:21 -0400

    Split up preview styles into separate stylesheet
    This commit splits out the Markdown styles into a separate stylesheet. This makes
    it easier for users to restyle the widget, since they can just override the path
    to martor-preview.css in the widget rather than hack Martor source.

--- martor/widgets.py
+++ martor/widgets.py
@@ -57,7 +57,8 @@ class MartorWidget(forms.Textarea):
                 'plugins/css/semantic.min.css',
                 'plugins/css/resizable.min.css',
                 'martor/css/martor.css',
-                'martor/css/martor-admin.css'
+                'martor/css/martor-admin.css',
+                'martor/css/martor-preview.css'
             )
         }
         js = (
